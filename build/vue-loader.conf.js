47e2af3b8f4b921d6d628bd9acfb2b593aaa63da
anthonykoch <anthonykoch@users.noreply.github.com>
2018-01-27 23:23:42 -0700

    Remove unnecessary build code

--- build/vue-loader.conf.js
+++ build/vue-loader.conf.js
@@ -10,25 +10,6 @@ module.exports = {
   loaders: utils.cssLoaders({
     sourceMap: sourceMapEnabled,
     extract: isProduction,
-    // scss: [
-    //   'vue-style-loader',
-    //   'css-loader',
-    //   {
-    //     loader: 'sass-loader',
-    //     options: {
-    //       includePaths: [
-    //         'src/styles',
-    //       ],
-    //     },
-    //   },
-    //   {
-    //     loader: 'sass-resources-loader',
-    //     options: {
-    //       // Provide path to the file with resources
-    //       resources: ['./src/styles/variables.scss'],
-    //     },
-    //   },
-    // ],
   }),
   cssSourceMap: sourceMapEnabled,
   cacheBusting: config.dev.cacheBusting,
@@ -36,6 +17,6 @@ module.exports = {
     video: ['src', 'poster'],
     source: 'src',
     img: 'src',
-    image: 'xlink:href'
+    image: 'xlink:href',
   }
 }
